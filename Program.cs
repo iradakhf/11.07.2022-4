4630fbc8abd3dd81f6bc9e10005c712335ed4645
iradakhf <iradakhf@code.edu.az>
2022-07-11 10:11:37 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ConsoleAp32
+{
+    class Porgram
+    {
+        public static void Main (string[] args)
+        {
+            Sheeps sheeps = new Sheeps(4, "sheep");
+           
+            sheeps.ShowTheNumberOfSheeps();
+        }
+        class Sheeps
+        {
+            public int NumberOfSheeps { get; set; }
+            public string Name { get; set; }
+            public Sheeps(int number, string name)
+            {
+                this.NumberOfSheeps = number;
+                this.Name = name;
+            }
+          
+            public void ShowTheNumberOfSheeps()
+            {
+               
+                    for (int i = 1; i <= this.NumberOfSheeps; i++)
+                    {
+                        Console.Write($"{i} : {this.Name} , ");
+                      
+                    }
+               
+                
+            }
+        }
+    }
+}
